cd6ece2716855ae5941182553dafd32ed912ec03
Markus Salgado <markus@Markuss-MacBook-Pro.local>
2021-06-08 22:02:03 -0400

    Creat is rendering

--- client/src/Components/Create.jsx
+++ client/src/Components/Create.jsx
@@ -1,50 +1,46 @@
-import { useState } from "react"
-import { createQuote } from "../services/api"
+import { useState } from "react";
+import { createQuote } from "../services/api";
 
 const newArt = {
   name: "",
   quote: "",
   dateMade: "",
-}
-
+};
 
 export default function CreateQuote() {
-  const [input, setInput] = useState(newArt)
+  const [input, setInput] = useState(newArt);
 
   const handleChange = (e) => {
-    const { name, value } = e.target
-    
-    setInput(prevInput => ({
+    const { name, value } = e.target;
+
+    setInput((prevInput) => ({
       ...prevInput,
       [name]: value,
-    }))
-  }
-
+    }));
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    const res = await createQuote()
-    console.log(res)
-  }
-
+    e.preventDefault();
+    const res = await createQuote(input);
+    console.log(res);
+  };
 
   return (
     <div className="Create">
-      <forms>
+      <h2>Contribute to The Kindess</h2>
+      <p>Just let your thoughts flow.</p>
+      <form onChange={handleChange} onSubmit={handleSubmit}>
         <label>Name: </label>
-          <input type="text" name="name" onChange={handleChange} />
-        
+        <input type="text" name="name" />
+
         <label> Quote:</label>
-        <input type="text" name="quote" onChange={handleChange} />
-        
+        <input type="text" name="quote" />
+
         <label>Date:</label>
-        <input type="text" name="dateMade" onChange={handleChange} />
-        
-        <button onSubmit={handleSubmit} type="submit">
-          Submit
-        </button>
-      </forms>
+        <input type="text" name="dateMade" />
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
-    
-  )
+  );
 }
